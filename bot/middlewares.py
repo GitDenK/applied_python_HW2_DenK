faadeef1c6cd19ac419b345872a8ef4bc73a88fe
GitDenK <deniskov.123@icloud.com>
2025-01-22 23:24:56 +0300

    fixed imports

--- bot/middlewares.py
+++ bot/middlewares.py
@@ -1,7 +1,17 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        print(f"Получено сообщение: {event.text}")
+        logger.info("Получено сообщение: %s", event.text)
         return await handler(event, data)
